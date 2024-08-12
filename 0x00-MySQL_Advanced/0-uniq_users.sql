78266107db692aab0a2e986fd50611abb62ff4dd
math198512 <thoraichi@gmail.com>
2024-08-12 12:07:52 +0100

    0-uniq_users.sql

---
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,7 @@
+-- Write a SQL script that creates a table users 
+-- with a id, email, and name columns.
+CREATE users (
+    id INT PRIMARY KEY,
+    email VARCHAR(255),
+    name VARCHAR(255)
+);
