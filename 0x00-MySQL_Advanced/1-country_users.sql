1991ac94db112a3933b1e4ab83a5e1dbd83b5fb0
math198512 <thoraichi@gmail.com>
2024-08-12 12:28:36 +0100

    1-country_users.sql

---
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,8 @@
+-- Write a SQL script that creates a table users 
+-- with a id, email, and name columns.
+CREATE table IF NOT EXISTS users (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    name VARCHAR(255),
+    country ENUM('US', 'CO', 'TN') DEFAULT 1
+);
