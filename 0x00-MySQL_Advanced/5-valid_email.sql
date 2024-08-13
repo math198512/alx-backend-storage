c490c3a6d9eb814f30c8fbb26b3ce7d7a3a7bdf5
math198512 <thoraichi@gmail.com>
2024-08-13 08:43:22 +0100

    5-valid_email.sql

--- 0x00-MySQL_Advanced/5-valid_email.sql
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -1,5 +1,5 @@
--- SQL script that creates a trigger that decreases
--- the quantity of an item after adding a new order.
+-- SQL script that creates a trigger that resets the attribute
+-- valid_email only when the email has been changed.
 
 CREATE TRIGGER reset_valid_email
 BEFORE UPDATE
