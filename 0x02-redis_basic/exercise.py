d668a65f3d74941fd4da15476d07e098111612f8
math198512 <thoraichi@gmail.com>
2024-08-14 19:12:23 +0100

    exercise.py

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -8,6 +8,7 @@ from functools import wraps
 
 UnionOfTypes = Union[str, bytes, int, float]
 
+
 def count_calls(method: Callable) -> Callable:
     """TO DO"""
     @wraps(method)
@@ -17,6 +18,7 @@ def count_calls(method: Callable) -> Callable:
 
         return method(self, *args, **kwargs)
     return wrapper
+
 
 class Cache:
     """simple Cache class"""
