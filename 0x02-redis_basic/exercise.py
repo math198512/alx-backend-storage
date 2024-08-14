230c853223561ca258ddef224644cb19f3191cbb
math198512 <thoraichi@gmail.com>
2024-08-14 16:27:07 +0100

    exercise.py

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -7,14 +7,12 @@ from uuid import uuid4
 class Cache:
     """simple Cache class"""
     def __init__(self) -> None:
-          """Constructor"""
-          self._redis = redis.Redis()
-          self._redis.flushdb()
+        """Constructor"""
+        self._redis = redis.Redis()
+        self._redis.flushdb()
 
     def Store(self, data) -> str:
         """Store method"""
         key = str(uuid4())
         self._redis.mset({key: data})
         return key
-         
-    
