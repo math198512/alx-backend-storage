84ab4dd724b4e5ca41fc49072b1de56019dd4b0b
math198512 <thoraichi@gmail.com>
2024-08-14 14:07:48 +0100

    10-update_topics.py

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -9,3 +9,5 @@ import pymongo
 def update_topics(mongo_collection, name, topics):
     """changes all topics of a school document based on the name"""
     mongo_collection.update_many({'name': name}, {'$set': {'topics': topics}})
+if __name__ == '__main__':
+    update_topics(mongo_collection, name, topics)
