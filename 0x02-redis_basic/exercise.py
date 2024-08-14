769f5651dbe247a33e180c5b09b207e259d1b280
math198512 <thoraichi@gmail.com>
2024-08-14 18:51:31 +0100

    exercise.py

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -20,3 +20,19 @@ class Cache:
         key = str(uuid4())
         self._redis.mset({key: data})
         return key
+
+    def get(self, key: str, fn: Optional[Callable] = None) -> UnionOfTypes:
+        """Reading from Redis and recovering original type"""
+        if fn:
+            data = fn(self._redis.get(key))
+        else:
+            data = self._redis.get(key)
+        return data
+
+    def get_int(self, key: str) -> int:
+        """Retrieves an integer value from a Redis data storage."""
+        return self.get(key: str, lambda x: int(x))
+
+    def get_str(self, key: str) -> str:
+        """Retrieves a string value from a Redis data storage."""
+        return self.get(key: str, lambda x: x.decode('utf-8'))
