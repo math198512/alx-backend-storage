ba25b0f0a296267f529ec19552d6e2ec6567138f
math198512 <thoraichi@gmail.com>
2024-08-14 13:10:23 +0100

    8-all.py

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -1,4 +1,10 @@
 #!/usr/bin/env python3
-"""a Python function that lists all documents in a collection"""
+"""a Python fupip3 install pymongonction that lists all documents in a collection"""
 import pymongo
 
+
+def list_all(mongo_collection):
+    """a Python function that lists all documents in a collection"""
+    if not mongo_collection:
+        return []
+    return mongo_collection.find()
